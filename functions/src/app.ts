d1cd47dcccfc03946643bbf0534ff971ee177799
Sriram Damodharan <sdamodharan@thapovan-inc.com>
2018-01-29 10:43:04 +0530

    Simple hello world

---
+++ functions/src/app.ts
@@ -0,0 +1,14 @@
+import * as cors from 'cors';
+import * as express from 'express';
+
+type Request = express.Request;
+type Response = express.Response;
+
+const app: express.Express = express();
+
+app.get('/helloworld', (request: Request, response: Response) => {
+    const name = request.query['name'] || 'world';
+    response.status(200).json({message: `Hello ${name}!!`});
+});
+
+export const APIService: express.Express = app;
